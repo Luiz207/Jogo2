c87355298c899320bb9ab9380bb5863e91a46d4a
Luiz Henrique <luizinfo.henrique9@gmail.com>
2018-03-15 10:48:42 -0300

    Segundo

---
+++ Assets/Scripts/GameBox.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameBox : MonoBehaviour {
+
+    private const int numColumnGrid = 4;
+    private const int numRowGrid = 9;
+
+    private float gridAreaWidth;
+    private float gridAreaHeight;
+    private Vector3 gridAreaPosition;
+
+    private float gameBlockWidth;
+    private float gameBlockHeight;
+
+      public static int NumColumnGrid
+    {
+        get { return numColumnGrid; }
+    }
+	// Use this for initialization
+	void Start () {
+     
+       Transform transformGridArea = transform.Find("GridArea");
+        SpriteRenderer spriteRendererGridArea = transformGridArea.gameObject.GetComponent<SpriteRenderer>();
+        gridAreaWidth = spriteRendererGridArea.bounds.size.x;
+        gridAreaHeight = spriteRendererGridArea.bounds.size.y;
+
+        gameBlockWidth = gridAreaWidth/ numColumnGrid;
+        gameBlockHeight = gridAreaHeight / numRowGrid;
+
+        gridAreaPosition = transformGridArea.position;
+        gridAreaPosition.z = 0f;
+
+        // Teste
+       /* for (int x=0; x < numColumnGrid; x++)
+        {
+            for (int y=0; y < numRowGrid; y++) {
+
+                GameObject gameBlockPrefab = Resources.Load("GameBlock") as GameObject;
+                GameObject gameBlock = Instantiate(gameBlockPrefab);
+                float posX = x * gameBlockWidth - (gridAreaWidth /2) + (gameBlockWidth /2);
+                float posY = y * gameBlockHeight - (gridAreaHeight/2) + (gameBlockHeight);
+                gameBlock.transform.position =
+                    new Vector3(posX, posY, 0f) + gridAreaPosition;
+                gameBlock.transform.SetParent(transform, false);
+            }
+        }*/
+	}
+	
+    public Vector3 GetGridPosition(int x , int y) {
+        float posX = x * gameBlockWidth - (gridAreaWidth / 2) + (gameBlockWidth / 2);
+        float posY = y * gameBlockHeight - (gridAreaHeight / 2) + (gameBlockHeight);
+        return new Vector3(posX, posY, -1f) + gridAreaPosition;
+
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/RespwnManeger.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class RespwnManeger : MonoBehaviour {
+
+    public GameBox gameBox;
+
+    private float respawnTimeCounter;
+
+    public float respawnTime;
+	// Use this for initialization
+	void Start () {
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        respawnTimeCounter += Time.deltaTime;
+        if (respawnTimeCounter >= respawnTime) {
+
+            Respawn();
+            respawnTimeCounter = 0;
+
+
+        }
+
+       }
+    private void Respawn()
+    {
+        GameObject gameBlockPrefab = Resources.Load("GameBlock") as GameObject;
+        GameObject gameBlock = Instantiate(gameBlockPrefab);
+        gameBlock.transform.SetParent(transform,false);
+        int randomColumn = Random.Range(0,GameBox.NumColumnGrid);
+        Debug.Log("Respawn Colunm" + randomColumn);
+        gameBlock.transform.position= gameBox.GetGridPosition(randomColumn,8);
+
+        
+    }
+
+ }
+
